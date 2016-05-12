b71789640a089637f5dfdee7cc64d1b86dfe9820
Andrew Shaffer <andrew@weo.io>
2016-05-12 12:03:29 -0700

    change fragment to params

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {fetch} from 'redux-effects-fetch'
 import identity from '@f/identity'
 import element from 'vdux/element'
 import map from '@f/map-obj'
+import qs from 'qs'
 
 /**
  * Config
@@ -37,82 +38,85 @@ const error = createAction('vdux-summon: request error')
  * Summon
  */
 
-function connect (fn, Ui) {
-  /**
-   * Component
-   */
+function connect (fn) {
+  return function (Ui) {
+    /**
+     * Component
+     */
 
-  const Component = {
-    initialState ({props, state}) {
-      return map((url, name) => ({
-        name,
-        loading: true,
-        error: null,
-        value: null
-      }), fn(props))
-    },
-
-    onCreate ({props, state, local, path}) {
-      return resolve(fn(props), state, local, path)
-    },
-
-    render ({props, state, children, local, path}) {
-      const mapping = fn(props)
-      const fns = {}
-
-      for (const key in mapping) {
-        if (typeof mapping[key] === 'function') {
-          fns[key] = (...args) => resolve(mapping[key](...args), state, local, path)
-        }
-      }
-
-      return (
-        <Ui {...state} {...fns} {...props}>
-          {children}
-        </Ui>
-      )
-    },
-
-    reducer: handleActions({
-      [loading]: (state, {key, url, reload}) => ({
-        ...state,
-        [key]: {
-          ...state[key],
-          url,
-          loaded: reload,
+    const Component = {
+      initialState ({props, state}) {
+        return map((url, name) => ({
+          name,
           loading: true,
-          reload
-        }
-      }),
-      [success]: (state, {key, value}) => ({
-        ...state,
-        [key]: {
-          ...state[key],
-          loading: false,
-          loaded: true,
-          value
-        }
-      }),
-      [error]: (state, {key, error}) => ({
-        ...state,
-        [key]: {
-          ...state[key],
-          loading: false,
-          error
-        }
-      })
-    }),
+          error: null,
+          value: null
+        }), fn(props))
+      },
 
-    onUpdate (prev, {props, state, local}) {
-      return resolve(fn(props), state, local)
-    },
+      onCreate ({props, state, local, path}) {
+        return resolve(fn(props), state, local, path)
+      },
 
-    onRemove ({path}) {
-      return unsubscribe(path)
+      render ({props, state, children, local, path}) {
+        const mapping = fn(props)
+        const fns = {}
+
+        for (const key in mapping) {
+          if (typeof mapping[key] === 'function') {
+            fns[key] = (...args) => resolve(mapping[key](...args), state, local, path)
+          }
+        }
+
+        return (
+          <Ui {...state} {...fns} {...props}>
+            {children}
+          </Ui>
+        )
+      },
+
+      reducer: handleActions({
+        [loading]: (state, {key, url, reload, params = {}}) => ({
+          ...state,
+          [key]: {
+            ...state[key],
+            url,
+            loaded: reload,
+            loading: true,
+            reload,
+            params
+          }
+        }),
+        [success]: (state, {key, value}) => ({
+          ...state,
+          [key]: {
+            ...state[key],
+            loading: false,
+            loaded: true,
+            value
+          }
+        }),
+        [error]: (state, {key, error}) => ({
+          ...state,
+          [key]: {
+            ...state[key],
+            loading: false,
+            error
+          }
+        })
+      }),
+
+      onUpdate (prev, {props, state, local}) {
+        return resolve(fn(props), state, local)
+      },
+
+      onRemove ({path}) {
+        return unsubscribe(path)
+      }
     }
-  }
 
-  return Component
+    return Component
+  }
 }
 
 /**
@@ -131,7 +135,7 @@ function *resolve (mapping, state, local, path) {
 
       if (descriptor.url && (method !== 'GET' || descriptor.url !== itemState.url)) {
         result.push(yield resolveUrl(key, descriptor, local, path))
-      } else if (descriptor.fragment) {
+      } else if (descriptor.params) {
         result.push(yield resolveFragment(key, descriptor, itemState, local, path))
       }
     }
@@ -203,15 +207,22 @@ function *resolveUrl (key, descriptor, local, path, reload = false) {
 }
 
 function *resolveFragment (key, descriptor, state, local, path) {
-  const {fragment, merge = defaultMerge, xf = identity} = descriptor
+  const {params, xf = identity} = descriptor
+  const merge = getMerge(descriptor.merge)
   const {url} = state
+  const mergedParams = {...(state.params || {}), ...params}
 
   if (!state.url) throw new Error('vdux-summon: Fragment can only be specified if an existing url has already been set for the collection')
 
   try {
-    yield local(loading)({url, key, reload: true})
+    yield local(loading)({
+      url,
+      key,
+      reload: true,
+      params: mergedParams
+    })
 
-    let {value} = yield fetch(getUrl(url + fragment))
+    let {value} = yield fetch(getUrl(url, qs.stringify(mergedParams)))
 
     value = merge(state.value, xf(value))
 
@@ -232,10 +243,16 @@ function *resolveFragment (key, descriptor, state, local, path) {
  * Helpers
  */
 
-function getUrl (url) {
-  return url[0] === '/'
+function getUrl (url, querystring) {
+  const resource = url[0] === '/'
     ? join(config.baseUrl, url)
     : url
+
+  if (!querystring) return resource
+
+  return resource.indexOf('?') !== -1
+    ? resource + '&' + querystring
+    : resource + '?' + querystring
 }
 
 function join (a, b) {
@@ -244,7 +261,26 @@ function join (a, b) {
     : a + b
 }
 
-function defaultMerge (prev = {items: []}, next) {
+function getMerge (merge) {
+  if (merge === undefined) merge = 'concat'
+
+  if (typeof merge === 'string') {
+    switch (merge) {
+      case 'replace':
+        return mergeReplace
+      case 'concat':
+        return mergeConcat
+    }
+  } else {
+    return merge
+  }
+}
+
+function mergeReplace (prev, next) {
+  return next
+}
+
+function mergeConcat (prev = {items: []}, next) {
   return {
     ...next,
     items: [...prev.items, ...next.items]
